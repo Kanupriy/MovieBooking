9c3350a8d500108c168c31fdb54f883032934819
Shivangi Jha <shivangijha89@gmail.com>
2020-10-27 00:30:42 +0530

    admin getter setters

--- MovieTicketBookingApp/src/main/java/com/cg/mts/entities/Admin.java
+++ MovieTicketBookingApp/src/main/java/com/cg/mts/entities/Admin.java
@@ -7,6 +7,24 @@ import javax.persistence.Id;
 public class Admin {
 @Id
 private int userId;
+public int getUserId() {
+	return userId;
+}
+public void setUserId(int userId) {
+	this.userId = userId;
+}
+public String getAdminName() {
+	return adminName;
+}
+public void setAdminName(String adminName) {
+	this.adminName = adminName;
+}
+public String getAdminContact() {
+	return adminContact;
+}
+public void setAdminContact(String adminContact) {
+	this.adminContact = adminContact;
+}
 private String adminName;
 private String adminContact;
 }
